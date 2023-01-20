77db8c22f7531ac668e293c1630c00fe9264288d
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:11:34 +0100

    Add project files.

---
+++ Array Inlämning 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            tal[0] = 5;
+            tal[1] = 9;
+            Console.WriteLine("Skriv in tredje heltalet.");
+            tal[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Här är de 3 talen");
+            Console.WriteLine($"{tal[0]}, {tal[1]}, {tal[2]}");
+
+        }
+    }
+}
+            
+
+
+
+
